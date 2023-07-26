ff9718bcb3a18cee5ba146225b6d7a7327e557ef
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-07-26 15:47:30 +0200

    first rest api endpoint

--- src/main/java/ay/education/springbootexample/SpringBootExampleApplication.java
+++ src/main/java/ay/education/springbootexample/SpringBootExampleApplication.java
@@ -2,12 +2,20 @@ package ay.education.springbootexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootExampleApplication.class, args);
 	}
 
+	@GetMapping("/greet")
+	public String greet() {
+		return "hello form first rest api";
+	}
 }
