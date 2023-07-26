e7211cfbdce8c34faa648f97ad58d7970d5ff0a5
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-07-26 16:57:49 +0200

    renamed start point of the application

--- src/main/java/ay/education/springbootexample/Main.java
+++ src/main/java/ay/education/springbootexample/Main.java
@@ -3,15 +3,14 @@ package ay.education.springbootexample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class SpringBootExampleApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootExampleApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 	@GetMapping("/greet")
