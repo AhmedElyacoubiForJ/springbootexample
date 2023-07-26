835271cbe7ecb55f7bc641df43d726634cf18b87
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-07-26 11:08:43 +0200

    project setup

---
+++ src/test/java/ay/education/springbootexample/SpringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ay.education.springbootexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
