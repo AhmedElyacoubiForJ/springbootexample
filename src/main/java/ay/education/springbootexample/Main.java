12c0510d1e92874cc34d6626ce396b6b290096c9
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-07-27 11:17:42 +0200

    diff. between class def. & record def. for response

--- src/main/java/ay/education/springbootexample/Main.java
+++ src/main/java/ay/education/springbootexample/Main.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @SpringBootApplication
 @RestController
 public class Main {
@@ -23,5 +25,38 @@ public class Main {
 		return new GreetResponse("hello, response as json object");
     }
 
-    record GreetResponse(String message) {}
+    // record GreetResponse(String message) {}
+
+	// class def. is the same thing as record def.
+	class GreetResponse {
+		private final String message;
+
+		public GreetResponse(String message) {
+			this.message = message;
+		}
+
+		public String getMessage() {
+			return message;
+		}
+
+		@Override
+		public String toString() {
+			return "GreetResponse{" +
+					"message='" + message + '\'' +
+					'}';
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			GreetResponse that = (GreetResponse) o;
+			return Objects.equals(message, that.message);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(message);
+		}
+	}
 }
