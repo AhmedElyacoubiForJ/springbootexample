a85a29c18ef16be51d43d0e10d79682d78148f51
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-07-28 23:57:33 +0200

    added new rest endpoint create new customer tested with postman

--- src/main/java/ay/education/springbootexample/Main.java
+++ src/main/java/ay/education/springbootexample/Main.java
@@ -4,9 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -54,6 +52,17 @@ public class Main {
         //return List.of();
         return customerRepository.findAll();
     }
+
+	record NewCustomerRequest(String name, String email, Integer age) {}
+
+	@PostMapping
+	public void addCustomer(@RequestBody NewCustomerRequest request) {
+		Customer customer = new Customer();
+		customer.setName(request.name);
+		customer.setEmail(request.email);
+		customer.setAge(request.age);
+		customerRepository.save(customer);
+	}
 
     @GetMapping("/greetAsString")
     public String greet() {
