47a563e5d9195bf33df4fd4f12f7a3be85881026
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-07-28 19:00:54 +0200

    database name changed & defined jpa annotations for the model class.

--- src/main/java/ay/education/springbootexample/Customer.java
+++ src/main/java/ay/education/springbootexample/Customer.java
@@ -1,8 +1,15 @@
 package ay.education.springbootexample;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.util.Objects;
 
+@Entity
 public class Customer {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
